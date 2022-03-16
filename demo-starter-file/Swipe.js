78e5b946478327e28424db0bd9182616c8890bff
William Wang <bn.william.w@gmail.com>
2022-03-16 17:33:58 -0400

    Working before class change

--- demo-starter-file/Swipe.js
+++ demo-starter-file/Swipe.js
@@ -5,6 +5,50 @@ import Card from "./Card";
 import carddata from "./carddata";
 import { useNavigation } from "@react-navigation/core";
 
+import { initializeApp } from "firebase/app";
+
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAsKEmxVNsnDUDxZJ7kwTZMk8ehDrOdlc0",
+
+  authDomain: "irly-demo.firebaseapp.com",
+
+  databaseURL: "https://irly-demo-default-rtdb.firebaseio.com",
+
+  projectId: "irly-demo",
+
+  storageBucket: "irly-demo.appspot.com",
+
+  messagingSenderId: "179945535098",
+
+  appId: "1:179945535098:web:a6f270ea7e3072ecb0edc3"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+const database = getDatabase(app);
+
+import { ref, child, get } from "firebase/database";
+
+let databaseReference = get(ref(getDatabase()));
+console.log(databaseReference);
+let data = [];
+
+
 class SwipeClass extends React.Component {
   render() {
     return <Swipe />;
@@ -12,18 +56,32 @@ class SwipeClass extends React.Component {
 }
 
 const Swipe = () => {
+
   const [index, setIndex] = useState(0);
   const onSwipe = () => {
     setIndex(index + 1);
   };
   const navigation = useNavigation();
+  databaseReference.then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log("Data available");
+      console.log(snapshot.val());
+      data = snapshot.val();
+      this.forceUpdate();
+      this.setState(this.state);
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
         <Swiper
           useViewOverflow={Platform.OS === "ios"}
-          cards={carddata}
+          cards={data}
           cardIndex={index}
           renderCard={(card) => <Card card={card} />}
           onSwiper={onSwipe}
